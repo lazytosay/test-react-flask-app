bbc0092b6f920370f3260febf68526456840dd34
lazytosay <siweisijiao.weebly@gmail.com>
2021-02-04 17:34:58 -0800

    update files

--- api/api.py
+++ api/api.py
@@ -1,7 +1,12 @@
 import time
 from flask import Flask
+import os
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.errorhandler(404)
